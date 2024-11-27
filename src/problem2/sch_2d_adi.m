f0c433a50936b0576a8b6ff1f61e519f5c4c2dd4
Gavin Pringle <gavinpringle00@gmail.com>
2024-11-26 21:37:02 -0800

    Completed well survey

--- src/problem2/sch_2d_adi.m
+++ src/problem2/sch_2d_adi.m
@@ -1,3 +1,5 @@
+% sch_2d_adi: Solves 2D Schrödinger equation using ADI scheme.
+% 
 % Inputs:
 %
 %   tmax:   Maximum integration time
@@ -20,3 +22,33 @@
 %   v:      Array of potential values [nx x ny]
 function [x y t psi psire psiim psimod v] = ...
     sch_2d_adi(tmax, level, lambda, idtype, idpar, vtype, vpar)
+
+    % Define mesh and derived parameters
+    nx = 2^level + 1;
+    ny = nx;
+    x = linspace(0.0, 1.0, nx);
+    y = linspace(0.0, 1.0, nx);
+    dx = x(2) - x(1);
+    dy = y(2) - y(1);
+    dt = lambda * dx;
+    nt = round(tmax / dt) + 1;
+    t = (0 : nt-1) * dt;
+
+    % Initialize solution, and set initial data
+    psi = zeros(nt, nx, ny);
+    if idtype == 0
+        % Exact family 
+        psi(1, :, :) = sin(idpar(1) * pi * x)' * sin(idpar(2) * pi * y);
+    elseif idtype == 1
+        % Boosted Gaussian
+        psi(1, :) = exp(1i * idpar(3) * x) .* ...
+                    exp(-((x - idpar(1)) ./ idpar(2)) .^ 2);
+    else
+        fprintf('sch_2d_adi: Invalid idtype=%d\n', idtype);
+        return
+    end
+    % Set first and last values of initial data to zero
+    psi(1, 1) = 0; 
+    psi(1, nx) = 0; 
+
+end
